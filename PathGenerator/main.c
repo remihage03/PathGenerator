fc7c499199b91917ae3986b6f5eabfa39e51c100
Rémi HAGE <remi.hage@student.junia.com>
2022-05-13 15:15:29 +0200

    idem

--- PathGenerator/main.c
+++ PathGenerator/main.c
@@ -9,7 +9,7 @@ int main(int argc, char* argv[])
 	if (argc != 5)
 	{
 		printf("\n[+] Usage: {height} {width} {difficulty} {filename}\n");
-		printf("[+] Difficulty: 1 = easy, 2 = medium, 3 = hard");
+		printf("[+] Difficulty: 1 = easy, 2 = medium, 3 = hard\n");
 		return EXIT_FAILURE;
 	}
 

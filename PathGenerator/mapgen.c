3688c9849e2016ad614d9db2f4b95d012d610b4a
Rémi HAGE <remi.hage@student.junia.com>
2022-05-13 09:57:14 +0200

    fonction countNeigbors refaite

--- PathGenerator/mapgen.c
+++ PathGenerator/mapgen.c
@@ -10,53 +10,27 @@ bool checkPos(Map* map, Vec2 pos)
 	return (pos.x >= 0 && pos.x < map->size.x && pos.y >= 0 && pos.y < map->size.y);
 }
 
-int countNeighbors(Map* map, Vec2 pos)
+int countNeighbors(Map* map, Vec2 pos) // Compte les voisins aux 4 points cardinaux de pos
 {
 	if (!map) return ERROR;
 
 	int nbNeighbors = 0;
-	int neighbors[4];
 
-	//printf("%d; %d\n", pos.x, pos.y);
-	//printf("%d\n", map->data[pos.x][pos.y]);
-	//for (int y = pos.y - 1; y <= pos.y +1 ; y++)
-	//{
-	//	for (int x = pos.x - 1; x <= pos.x + 1; x++)
-	//	{
-	//		if (!(x != pos.x && y != pos.y))
-	//			printf("%c ", map->data[x][y] == 0 ? 'O' : '@');
-	//	}
-	//	printf("\n");
-	//}
-	Vec2 _pos = pos;
-
-	_pos.y = pos.y - 1;
-	if (checkPos(map, _pos))
-		neighbors[0] = map->data[_pos.x][_pos.y];
-	else neighbors[0] = 0;
-
-	_pos.y = pos.y + 1;
-	if (checkPos(map, _pos))
-		neighbors[1] = map->data[_pos.x][_pos.y];
-	else neighbors[1] = 0;
-
-	_pos = pos;
-	_pos.x = pos.x - 1;
-	if (checkPos(map, _pos))
-		neighbors[2] = map->data[_pos.x][_pos.y];
-	else neighbors[2] = 0;
-
-	_pos.x = pos.x + 1;
-	if (checkPos(map, _pos))
-		neighbors[3] = map->data[_pos.x][_pos.y];
-	else neighbors[3] = 0;
-
-	for (int i = 0; i < 4; i++)
+	for (int y = pos.y - 1; y <= pos.y +1 ; y++)
 	{
-		if (neighbors[i] == 1)
-			nbNeighbors++;
+		Vec2 _pos = { pos.x, y };
+		if (checkPos(map, _pos))
+			if (y != pos.y && map->data[pos.x][y] == 1)
+				nbNeighbors++;
 	}
-	//printf("%d\n", nbNeighbors);
+
+	for (int x = pos.x - 1; x <= pos.x + 1; x++)
+	{
+		Vec2 _pos = { x, pos.y };
+		if (checkPos(map, _pos))
+			if (x != pos.x && map->data[x][pos.y] == 1)
+				nbNeighbors++;
+	}
 
 	return nbNeighbors;
 }
@@ -78,7 +52,7 @@ Map* genMap(Map* map, Vec2 size, Difficulty diff)
 	map->size = size;
 	map->level = diff;
 
-	map->data = (int*)calloc(size.x, sizeof(int*)); // Malloc des x
+	map->data = (int**)calloc(size.x, sizeof(int*)); // Malloc des x
 	for (int i = 0; i < size.x; i++)
 		map->data[i] = (int*)calloc(size.y, sizeof(int)); // Malloc des y
 
@@ -169,7 +143,7 @@ void printMap(Map* map)
 		for (int x = 0; x < map->size.x; x++)
 		{
 			printf("%c ", map->data[x][y] == 0 ? ' ' : '@');
-			//printf("2%d ", map->data[x][y]);
+			//printf("%d ", map->data[x][y]);
 		}
 		printf("\n");
 	}

eba7179036d2174538121167f585cf04d1fad272
Rémi HAGE <remi.hage@student.junia.com>
2022-06-01 16:31:25 +0200

    export

--- PathGenerator/main.c
+++ PathGenerator/main.c
@@ -2,11 +2,11 @@
 #include <time.h>
 #include <stdlib.h>
 #include "mapgen.h"
-#include "mapgen.c"
+//#include "mapgen.c"
 #include "solver.h"
-#include "solver.c"
+//#include "solver.c"
 #include "stack.h"
-#include "stack.c"
+//#include "stack.c"
 
 #include "graph_sacha.c"
 #include "graph_sacha.h"
@@ -35,7 +35,7 @@ int main(int argc, char* argv[])
 	print_shard(map,&printPath);
 	export_map(map,"file.json");
 
-	printf("\n20 : %d,21 %d <=> en ratio : 20 = %f, 21 = %f",total20,total21,(float)total20/(float)ite,(float)total21/(float)ite);
+	/*printf("\n20 : %d,21 %d <=> en ratio : 20 = %f, 21 = %f",total20,total21,(float)total20/(float)ite,(float)total21/(float)ite);
 	printf("\naugmentation : %f pourcents sur %d iterations",(float)total21/(float)total20,ite);
 	
 	
@@ -53,30 +53,9 @@ int main(int argc, char* argv[])
 	map_to_solve->path = (Vec2*)malloc(sizeof(Vec2)*50);
 	printf("mapcount %d",map_to_solve->path_count);
 	printf("\n return value : %d",Solve(map_to_solve,1,1,osef));
-
 	print_shard(map_to_solve,&printPath);
-	// for(int i =0;i<map_to_solve->path_count;i++){
-	// 	printf("\n x : %d - y : %d ",map_to_solve->path[i].x,map_to_solve->path[i].y);
-	// }
 	draw_path(map_to_solve);
-	print_shard(map_to_solve,&printPath);
-
-	// int res = map->size.x * map->size.y;
-	// graph* g = create_graph(res);
-
-	// for (int y = 1; y < map->size.y - 1; y++)
-	// {
-	// 	for (int x = 1; x < map->size.x - 1; x++)
-	// 	{
-	// 		Vec2 pos = {x, y};
-	// 		create_neighbor_list(g, map, pos);
-	// 	}
-	// }
-
-	// export_map(map, argv[4]);
-
-	// print_graph(g);
-	// destroy_graph(g);
+	print_shard(map_to_solve,&printPath);*/
 
 	return EXIT_SUCCESS;
 
